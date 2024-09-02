eccd57fe7ba0cdbd37940baa6265b8666ec3ec31
Silvia Mota <aa.silvia.mota@gmail.com>
2024-09-02 10:02:25 +0200

    fixed styling, indentation and some frontend for home page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,14 +1,11 @@
 class Restaurant < ApplicationRecord
-
   has_many :recommendations, dependent: :destroy
   has_many :wishlists, dependent: :destroy
 
-
   include PgSearch::Model
   pg_search_scope :search_by_location,
-  against: [ :city, :location ],
-  using: {
-    tsearch: { prefix: true }
-  }
-
+                  against: %i[city location],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
